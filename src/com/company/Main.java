f4538e28c9305a78e6f29048901acc4e4a006550
Mahindra <mahindrabhagat98@gmail.com>
2022-02-21 10:25:49 +0530

    bablu done it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        ok muk=new ok();
+        System.out.println(muk.mahi(10));
         System.out.println("THIS IS TESTING PROGRAMME");
     }
+    static class ok {
+        int x;
+
+        public int mahi(int x) {
+            int area = x * x;
+            return area;
+        }
+    }
+
 }
+
